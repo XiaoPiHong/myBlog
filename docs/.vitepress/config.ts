9898c80001c0d28fbc8e9cd17a9984b02fb72dbf
XiaoPiHong <260094892@qq.com>
2023-02-14 11:31:29 +0800

    重写vite篇章

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
         text: "前端工程化",
         collapsible: true,
         items: [
-          { text: "vite", link: "/engineering/engineering-vite" },
+          // { text: "vite", link: "/engineering/engineering-vite" },
           {
             text: "vuecli5+vue2+ts",
             link: "/engineering/engineering-vuecli5-vue2-ts",
